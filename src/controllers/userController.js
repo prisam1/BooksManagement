35c47917e430b27586f29ab39de7ee5484f2f5ab
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-09-20 15:11:04 +0530

    RD

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,14 +7,20 @@ const login = async function (req, res) {
     let { email, password }=req.body
     if (!Validator.checkInputsPresent(req.body)) return res.status(400).send({ status: false, message: "Data must be present" })
     if (!email) return res.status(400).send({ status: false, message: "EmailId is mandatory" })
+    if(!Validator.validateEmail(email)){
+        return res.status(400).send({ status: false, message: "EmailId is invalid" })
+    }
     
     if (!password) return res.status(400).send({ status: false, message: "Password is mandatory" })
+    if(!Validator.validPassword(password)){
+        return res.status(400).send({ status: false, message: "Password is invalid" })
+    }
     let loginUser = userModel.findOne({ email: email, password:password })
     if (!loginUser) {
         return res.status(401).send({ status: false, message: "Login failed due to incorrect password or email" })
     }
    let token = jwt.sign({
-        userId: data["_id"].toString()
+        userId: loginUser["_id"].toString()
     }, "This is our Secret", {
         expiresIn: '20s' // expires in 20 seconds
     });
@@ -31,7 +37,7 @@ const createUser=async function(req,res){
     try{
         let body=req.body;
         if(checkInputsPresent(body)){
-            
+
         }
         
 
