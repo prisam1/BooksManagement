6a591e53945c7bc41653f009f60e6b6f966d7a8a
deepak9664 <singhdepak30@gmail.com>
2022-09-20 12:49:43 +0530

    loginchanges

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,2 +1,22 @@
 const userModel=require('../models/userModel.js')
 
+const login = function (req, res) {
+    try{
+    let body = req.body;
+    let data = userModel.findOne({ email: body.email, password: body.password })
+    if (!data) {
+        return res.status(401).send({ status: false, message: "Login failed due to incorrect password or email" })
+    }
+   let token = jwt.sign({
+        userId: data["_id"].toString()
+    }, "This is our Secret", {
+        expiresIn: '20s' // expires in 20 seconds
+    });
+    return res.status(200).send({status:true,message:"Success",data: {token:token}})
+}
+catch(err){
+    return res.status(401).send({ status: false, message: err.message })
+
+}
+}
+
