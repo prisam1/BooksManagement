a63b9a94fa5bdd8277c6a1b8c21d3eaa31281424
deepak9664 <singhdepak30@gmail.com>
2022-09-20 12:46:57 +0530

    validation

---
+++ src/validation/validfun.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+const checkInputsPresent = (value) => {
+    return (Object.keys(value).length > 0);
+}
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || typeof value === null) return false
+    if (typeof value === "string" && value.trim().length == 0) return false
+    return true
+}
+
+// const isTitleValid= function(value){
+//     if (value == "Mr" ||value =="Miss" ||value =="Mrs") return true
+//     return false
+// }
+const isValidTitle = function (title,inp) {
+    return inp.indexOf(title) !== -1;
+  };
+
+const validateEmail = (email) => {
+    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
+}
+const validateMobileNo = (number) => {
+    return (/^[0-9]{10}$/.test(number))
+    
+}
+module.exports={checkInputsPresent,isValid,isValidTitle,validateMobileNo,validateEmail}
